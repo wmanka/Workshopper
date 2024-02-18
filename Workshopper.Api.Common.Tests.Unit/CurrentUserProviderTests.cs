365982f8b58f40abf9c7f96d8bb51d989c5449aa
wioletta.manka <wioletta.manka728@gmail.com>
2024-02-18 11:57:29 +0100

    Add current user provider tests

---
+++ Workshopper.Api.Common.Tests.Unit/CurrentUserProviderTests.cs
@@ -0,0 +1,84 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using NSubstitute.Extensions;
+using NSubstitute.ReturnsExtensions;
+using Workshopper.Application.Common.Models;
+using Workshopper.Domain.Users.UserProfiles;
+using Workshopper.Infrastructure.Authentication;
+
+namespace Workshopper.Api.Common.Tests.Unit;
+
+public class CurrentUserProviderTests
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly CurrentUserProvider _currentUserProvider;
+
+    public CurrentUserProviderTests()
+    {
+        _httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        _currentUserProvider = new CurrentUserProvider(_httpContextAccessor);
+    }
+
+    [Fact]
+    public void GetCurrentUser_ReturnCurrentUser_WhenCurrentUserExists()
+    {
+        var userId = Guid.NewGuid();
+        // var profileId = Guid.NewGuid();
+        _httpContextAccessor.Configure().HttpContext.Returns(new DefaultHttpContext
+        {
+            User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(CustomClaimType.UserId, userId.ToString()),
+                // new Claim(CustomClaimType.ProfileId, profileId.ToString())
+            }))
+        });
+
+        var result = _currentUserProvider.GetCurrentUser();
+
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new
+        {
+            UserId = userId
+        });
+    }
+
+    [Fact]
+    public void GetCurrentUser_ReturnNull_WhenCurrentUserDoesnNotExists()
+    {
+        _httpContextAccessor.Configure().HttpContext.ReturnsNull();
+
+        var result = _currentUserProvider.GetCurrentUser();
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void GetCurrentUser_ReturnCurrentUserWithProfileData_WhenCurrentUserIsLoggedInSpecificProfile()
+    {
+        var userId = Guid.NewGuid();
+        var profileId = Guid.NewGuid();
+        var profileType = ProfileType.Host;
+        const string role = DomainRoles.Host;
+        _httpContextAccessor.Configure().HttpContext.Returns(new DefaultHttpContext
+        {
+            User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(CustomClaimType.UserId, userId.ToString()),
+                new Claim(CustomClaimType.ProfileId, profileId.ToString()),
+                new Claim(CustomClaimType.ProfileType, profileType.Name),
+                new Claim(CustomClaimType.Role, role)
+            }))
+        });
+
+        var result = _currentUserProvider.GetCurrentUser();
+
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(new
+        {
+            UserId = userId,
+            ProfileId = profileId
+        });
+
+        result!.IsHost.Should().BeTrue();
+    }
+}
--- Workshopper.Infrastructure/Internals.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("Workshopper.Infrastructure.Tests.Unit")]
